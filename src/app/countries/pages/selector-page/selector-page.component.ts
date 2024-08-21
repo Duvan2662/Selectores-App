77569fed54c0e347306f7fa5adca5f48119ad19c
Duvan <duvanriano2626@gmail.com>
2024-08-21 16:25:07 -0500

    Creacion del primer formulario usando bootstrap

--- src/app/countries/pages/selector-page/selector-page.component.ts
+++ src/app/countries/pages/selector-page/selector-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'countries-selector-page',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class SelectorPageComponent {
 
+  public myForm:FormGroup = this.fb.group({
+    region:['',Validators.required],
+    coountry:['',Validators.required],
+    limites:['',Validators.required],
+
+  })
+
+  constructor(private fb : FormBuilder){
+
+  }
 }
